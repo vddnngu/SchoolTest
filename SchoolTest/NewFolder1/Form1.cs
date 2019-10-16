535245b11bb9e42edf89f68bc0f6b06d4b84fe69
dvolkov <vdddzer152@yandex.ru>
2019-10-16 13:26:57 +0300

    Test commit

--- SchoolTest/NewFolder1/Form1.cs
+++ SchoolTest/NewFolder1/Form1.cs
@@ -16,5 +16,15 @@ namespace SchoolTest
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Кнопка нажата!";
+        }
     }
 }
